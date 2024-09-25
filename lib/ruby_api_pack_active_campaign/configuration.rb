7915621b823651f8b875707e1dd87822b22c39c4
BradPotts <brad.potts@coastdigitalgroup.com>
2024-09-26 02:01:07 +0800

    drafted out work.

--- lib/ruby_api_pack_active_campaign/configuration.rb
+++ lib/ruby_api_pack_active_campaign/configuration.rb
@@ -2,12 +2,11 @@
 
 module RubyApiPackActiveCampaign
   class AcConfiguration
-    attr_accessor :ac_api_url, :ac_api_token, :ac_account_name
+    attr_accessor :ac_api_url, :ac_api_token
 
     def initialize
-      @ac_api_url = 'https://<your-account>.api-us1.com/api/3' # Default base URL
+      @ac_api_url = 'https://youraccountname.api-us1.com/api/3'
       @ac_api_token = nil
-      @ac_account_name = nil
     end
   end
 end
