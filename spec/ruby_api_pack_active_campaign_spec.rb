99025f6afe1227d603ce0b9dc868af3538e7d47f
BradPotts <brad.potts@coastdigitalgroup.com>
2024-09-26 01:45:07 +0800

    Start work building the connection

--- spec/ruby_api_pack_active_campaign_spec.rb
+++ spec/ruby_api_pack_active_campaign_spec.rb
@@ -1,11 +1,1 @@
 # frozen_string_literal: true
-
-RSpec.describe RubyApiPackActiveCampaign do
-  it 'has a version number' do
-    expect(RubyApiPackActiveCampaign::VERSION).not_to be_nil
-  end
-
-  it 'does something useful' do
-    expect(false).to be(true)
-  end
-end
